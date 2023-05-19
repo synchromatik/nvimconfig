a8ace6a1fe7b30de258d00870a1dfdfc36fe00db
M.M <synchromatik@gmail.com>
2023-05-19 15:26:57 +0200

    initial migration config

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,62 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  --
+  -- themes
+      { "https://gitlab.com/__tpb/monokai-pro.nvim" },
+      { "tiagovla/tokyodark.nvim" },
+      { "navarasu/onedark.nvim" },
+      { "shaunsingh/moonlight.nvim" },
+      { "shaunsingh/nord.nvim" },
+      { "EdenEast/nightfox.nvim" },
+      {
+        "folke/tokyonight.nvim",
+        config = function()
+          require("tokyonight").setup({
+            styles = {
+              -- Style to be applied to different syntax groups
+              -- Value is any valid attr-list value for `:help nvim_set_hl`
+              comments = { italic = false },
+              keywords = { italic = false },
+            },
+          })
+        end,
+      },
+      { "catppuccin/nvim" },
+      -- plugins
+      -- Outline
+      {
+        "simrat39/symbols-outline.nvim",
+        config = function()
+          require("symbols-outline").setup()
+        end,
+      },
+      -- Trouble :Trouble
+      { "folke/trouble.nvim" },
+      -- TODO Comments
+      {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = function()
+          require("todo-comments").setup()
+        end,
+      },
+      -- NAVIC path
+      {
+        "SmiteshP/nvim-navic",
+        requires = "neovim/nvim-lspconfig",
+        config = function()
+          require("nvim-navic").setup()
+        end,
+      },
+      -- Floating CMD line :cmdline
+      {
+        'VonHeikemen/fine-cmdline.nvim',
+        requires = {
+          { 'MunifTanjim/nui.nvim' }
+        },
+        config = function()
+          require("fine-cmdline").setup()
+        end,
+      },
 }
